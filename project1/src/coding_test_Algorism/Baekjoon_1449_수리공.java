387781438271c798f7d3e526969ef1b46559ab9a
whymo <mingocorea@stu.ac.kr>
2022-07-20 00:53:36 +0900

    백준 1449 수리공 - 설명 추가

--- project1/src/coding_test_Algorism/Baekjoon_1449_수리공.java
+++ project1/src/coding_test_Algorism/Baekjoon_1449_수리공.java
@@ -32,19 +32,27 @@ public class Baekjoon_1449_수리공 {
 		} // for
 		
 		// 배열을 오름차순으로 정렬하기
+		// 1 5 9 2 6 4 이렇게 들어오면 정확한 계산이 불가능하기에 정렬이 필요하다.
 		Arrays.sort(arr);
 		
+		// 테이프의 수
 		int count = 1;
+		
 		int idx = 0;
 		
 		for ( int i = idx + 1; i < arr.length; i++ ) {
 			
+			// arr[idx]의 의미는 arr 0번째인 첫번째 원소부터 테이프를 붙이면
+			// 양쪽 끝에 0.5씩 여유분을 두어야 하기 때문에 arr[0] + 테이프 길이 -1만큼 붙일 수 있다.
+			// 만약 테이프의 길이가 끝났는데, 다음 원소가 존재할 경우 +1을 해줘야 한다.
 			if ( ( arr[idx] + L - 1 ) < arr[i] ) {
+				
 				count ++;
 				idx = i;
+				
 			} // inner for
 			
-		} // outer for
+		} // outer for : 원소 1부터 시작하는 이유는 0번부터 붙이기에 0번은 당연히 테이프가 붙기에 0번은 무시가 가능하다.
 		
 		System.out.println(count);
 		

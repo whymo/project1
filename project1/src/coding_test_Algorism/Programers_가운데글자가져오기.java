2209a28dcb2e5dab066c9453de67a188d8a65f22
whymo <mingocorea@stu.ac.kr>
2022-07-31 17:27:56 +0900

    프로그래머스 가운데 글자 가져오기 - substring(start index, end index) 끝인덱스번호는 안됨

---
+++ project1/src/coding_test_Algorism/Programers_가운데글자가져오기.java
@@ -0,0 +1,53 @@
+package coding_test_Algorism;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Programers_가운데글자가져오기 {
+	
+	// =========================================================
+    // 문제 :
+    // 단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요.
+    // 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
+    // =========================================================
+    
+    public String solution(String s) {
+        
+        String answer = "";
+        
+        int middle = s.length() / 2;
+        
+        if ( s.length() % 2 == 0 ) {
+            
+            // 짝수라면
+            // 문자열.substring(시작인덱스번호, 끝인덱스번호); (****)
+            // 끝 인덱스 번호는 포함되지 않는다.
+            // 시작 인덱스는 포함이 된다.
+            answer = s.substring( middle - 1, middle + 1 );
+        
+        } else {
+            
+            // 홀수라면
+            answer = s.substring( middle, middle+1 );
+        
+        } // if : 짝수일때는 2글자 홀수일때는 1글자를 반환
+        
+        return answer;
+        
+    } // solution
+	
+    // =========================================================
+    
+	public static void main (String[]args) throws Exception {
+		
+		Programers_가운데글자가져오기 test = new Programers_가운데글자가져오기();
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		
+		System.out.println(test.solution(s));
+		
+	} // main
+
+} // end class

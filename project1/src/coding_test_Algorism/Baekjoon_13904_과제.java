253b2ebc36672a8593efa54ed8eb65b91407c274
whymo <mingocorea@stu.ac.kr>
2022-08-24 23:54:26 +0900

    백준 13904 과제

---
+++ project1/src/coding_test_Algorism/Baekjoon_13904_과제.java
@@ -0,0 +1,33 @@
+package coding_test_Algorism;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon_13904_과제 {
+	
+	public static void main(String[] args) throws Exception {
+		
+		// ==============================================================
+		// 문제 :
+		// + 웅찬이는 과제가 많다. 하루에 한 과제를 끝낼 수 있는데, 
+		// + 과제마다 마감일이 있으므로 모든 과제를 끝내지 못할 수도 있다.
+		// + 웅찬이는 가장 점수를 많이 받을 수 있도록 과제를 수행하라
+		// ==============================================================
+		// + 첫 줄에 정수 N (1 ≤ N ≤ 1,000)이 주어진다. ( 과제의 수 )
+		// + 다음 줄부터 N개의 줄에는 각각 두 정수 d와 w가 주어진다.
+		// + d는 과제 마감일까지 남은 일수를 의미하며,
+		// + w는 과제의 점수를 의미한다.
+		// ==============================================================
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		// + 1. 과제의 수를 입력받는다.
+		int N = Integer.parseInt(br.readLine());
+		
+		// + 2. 과제를 저장하는 리스트 객체를 받는다.
+	
+		
+	} // main
+
+} // end class
